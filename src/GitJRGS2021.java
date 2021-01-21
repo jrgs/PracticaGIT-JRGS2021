f85eb66e14a31b9d09fb069bcc6328427913c680
Jose Ramón García Sevilla <jrgarcia@iesmarenostrum.com>
2021-01-21 12:04:11 +0100

    Versión inicial del proyecto

---
+++ src/GitJRGS2021.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GitJRGS2021 {
+    private JPanel GitPanelJRGS2021;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GitJRGS2021");
+        frame.setContentPane(new GitJRGS2021().GitPanelJRGS2021);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
